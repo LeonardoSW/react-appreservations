cf217b737863a7f4d5df743b241c026216c394ab
Leonardo <leonardo@LAPTOP-2S3MJ3JD>
2021-12-11 18:46:21 -0300

    added image, creation of the unitary reserva component

--- src/components/Reserva/index.js
+++ src/components/Reserva/index.js
@@ -3,8 +3,45 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Reserva() {
     return(
-        <View>
-            <Text>AAAAAAAAAAAAAAAAAAAAAAAAABBBCCCCCCC</Text>
+        <View style={styles.background}>
+
+            <View style={styles.container}>
+                <Image
+                style={styles.containerimg}
+                source={require('../../img/local.jpg')}
+                />
+            </View>
         </View>
     )
 }
+
+
+
+export const styles = StyleSheet.create({
+    background: {
+      flex: 1,
+      backgroundColor: '#5e8d69',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    container:{
+        width: '90%',
+        backgroundColor: '#77af88',
+        maxHeight:'13%',
+        maxWidth:'90%',
+        borderRadius:20,
+        alignItems: 'flex-end',
+        display:'flex',
+    },
+    containerimg:{
+        maxHeight:'100%',
+        maxWidth:'25%',
+        borderRadius:20,
+        display:'flex',
+    },
+    containerText:{
+        alignItems: 'center',
+        width:'90%'
+    }
+
+  })
