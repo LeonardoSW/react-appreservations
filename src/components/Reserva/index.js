35bcbfd6bd3a7ed3fd217a643c5ca2d6b8f79397
Leonardo <leonardo@LAPTOP-2S3MJ3JD>
2021-12-12 01:01:40 -0300

    finished reserva component and using in page Reservas, reserva not dynamic for while, but fix in code to tests

--- src/components/Reserva/index.js
+++ src/components/Reserva/index.js
@@ -3,21 +3,35 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Reserva() {
     return(
-        <View style={styles.background}>
+  /*      <View style={styles.global}>*/
 
             <View style={styles.container}>
                 <Image
                 style={styles.containerimg}
                 source={require('../../img/local.jpg')}
                 />
+                <View style={styles.viewtexts}>
+                    <Text style={styles.containertext}>Stéphany Amaoka</Text>
+                    <Text style={styles.containertext2}>Salão de Festas</Text>
+                    <Text style={styles.containertext3}>Quadra 2 - 19/08 | 20h</Text>
+                </View>
             </View>
-        </View>
+
+ /*       </View>*/
     )
 }
 
 
 
 export const styles = StyleSheet.create({
+    global:{
+        marginTop: 30,
+        backgroundColor: '#5e8d69',
+        alignItems:'center',
+        flex:1,
+        display:'flex',
+        flexDirection:'column'
+    },
     background: {
       flex: 1,
       backgroundColor: '#5e8d69',
@@ -25,13 +39,15 @@ export const styles = StyleSheet.create({
       justifyContent: 'center',
     },
     container:{
+        marginTop:20,
         width: '90%',
         backgroundColor: '#77af88',
         maxHeight:'13%',
         maxWidth:'90%',
         borderRadius:20,
-        alignItems: 'flex-end',
+        alignItems: 'flex-start',
         display:'flex',
+        flexDirection: 'row-reverse',
     },
     containerimg:{
         maxHeight:'100%',
@@ -39,9 +55,25 @@ export const styles = StyleSheet.create({
         borderRadius:20,
         display:'flex',
     },
-    containerText:{
-        alignItems: 'center',
-        width:'90%'
+    viewtexts:{
+        display:'flex',
+        flexDirection:'column',
+        marginTop:1,
+    },
+    containertext:{
+        width: 250,
+        color:'#FFF',
+        fontSize:25,        
+    },
+    containertext2:{
+        width: 250,
+        color:'#FFF',
+        fontSize:20,    
+    },
+    containertext3:{
+        width: 250,
+        color:'#FFF',
+        fontSize:15,    
     }
 
   })
--- src/pages/Reservations/index.js
+++ src/pages/Reservations/index.js
@@ -5,10 +5,33 @@ import Reserva from '../../components/Reserva';
 
 /* import Style from './style';*/
 
-export default function Reservas() {
+export default function Reservas(qtd) {
     return (
-        <Reserva/>      
+        <View style={styles.background}>
+            <Reserva/>
+            <Reserva/>
+            <Reserva/>
+            <Reserva/>
+            <Reserva/>
+            <Reserva/>
+            <Reserva/>
+            <Reserva/>
+            <Reserva/>
+        </View>
     );
   }
 
-  
+  function CarregaReservas(){
+      for( i = 0; i < 10; i++){
+        <Reserva/>
+      }
+  }
+
+  export const styles = StyleSheet.create({
+    background: {
+      flex: 1,
+      backgroundColor: '#5e8d69',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+})
